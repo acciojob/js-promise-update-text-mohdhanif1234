fbf9e9bca48984e0da1d0e6b50f76f52be35f3b3
mohdhanif1234 <88095538+mohdhanif1234@users.noreply.github.com>
2023-06-23 00:19:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function myFunc(){
+	return new Promise((resolve)=>{
+		setTimeout(()=>{
+			resolve("Hello, world!");
+		},1000)
+	})
+}
+
+myFunc().then((val)=>{
+	document.getElemenById("output").textContent = val
+})
+
